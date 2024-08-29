b97307b2f8b9e2a2424ac6eef36a431f882f6092
Auziwill <167850689+Auziwill@users.noreply.github.com>
2024-08-29 08:19:35 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,29 @@
+<div class="container-fluid py-5 content-subscribe text-light">
+        <div class="container">
+            <h5 class="text-center mb-4">Temui Kami</h5>
+            <div class="row justify-content-center">
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="https://instagram/auziwill"><i class="fab fa-instagram fs-2"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="https://facebook"><i class="fab fa-facebook fs-2"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="https://twitter"><i class="fab fa-twitter fs-2"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                    <a href="https://telegram"> <i class="fab fa-telegram fs-2"></i></a>
+                </div>
+                <div class="col-sm-1 d-flex justify-content-center mb-2">
+                   <a href="https://wa.me/085701917804"> <i class="fab fa-whatsapp fs-2"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div>
+        <div class="container d-flex justify-content-between">
+            <label>&copy;2024 kafe saya</label>
+            <label>Created by wildan</label>
+        </div>
+    </div>
---
+++ tentang-kami.php
@@ -0,0 +1,31 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko Online || Tentang kami</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+        <?php require "navbar.php"; ?>
+
+        <div class="container-fluid banner2 d-flex align-items-center">
+        <div class="container">
+            <h1 class="text-white text-center">tentang Kami</h1>
+        </div>
+        </div>   
+        <div class="container-fluid py-5">
+            <div class="container fs-5">
+                <p>apasi yang ga buat kamuuuuuuu</p>
+            </div>
+        </div> 
+
+        <?php require "footer.php";?>
+        <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="fontawesome/js/all.min.js"></script>  
+</body>
+</html>
